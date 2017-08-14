cb3746b43aa03305726987c98fc079ed2e3434f6
John T Cronin <jt.cronin@outlook.com>
2017-08-14 14:32:25 -0500

    finished six

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,8 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function(){
+
+	var input = document.getElementById('full_name');
+	input.addEventListener('keyup', function(){
+		var output = document.getElementById('greeting');
+		output.innerHTML = ("Hello there " + input.value + "!");
+	});
+});
