e5a2d68012c333d0178c5d41f8d8c919ac01468d
John T Cronin <jt.cronin@outlook.com>
2017-08-14 15:08:07 -0500

    finished all problems as well as refactored the last one

--- 07-automatic-tabbing-fields/QUESTIONS.md
+++ 07-automatic-tabbing-fields/QUESTIONS.md
@@ -6,4 +6,4 @@
 
 > What are some refactoring techniques you might apply to the provided solution?
 
-Your reply here...
+We could make another function that will see if the input text is equal to the max length. We repeat these steps twice so we could do that to refactor.
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,28 @@
-// Your JavaScript goes here.
+window.addEventListener('load', function() {
+	 var first = document.getElementById('first');
+	 var second = document.getElementById('second');
+	 var third = document.getElementById('third');
+
+
+	 function check(inputed){
+	 	typed = inputed.value.length;
+	 	max = parseInt(inputed.getAttribute("maxlength"));
+	 	if (typed === max);
+
+	 	return true;
+	 }
+
+	first.addEventListener("keyup", function() {
+	if (check(first)){
+		second.focus();
+	}
+  	});
+
+	second.addEventListener("keyup", function() {
+	if (check(second)){
+		third.focus();
+	}
+	});
+
+	 
+});
